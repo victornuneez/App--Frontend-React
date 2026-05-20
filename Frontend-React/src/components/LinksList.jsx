adafa533b067411b78eb2d963fc94693de5e3a15
Victor Nunez <tu_correo@gmail.com>
2026-05-20 19:34:01 -0300

    feat: agregar funcionalidad a los botones de ver mas, editar y eliminar enlaces

--- Frontend-React/src/components/LinksList.jsx
+++ Frontend-React/src/components/LinksList.jsx
@@ -1,13 +1,16 @@
 
 // Creamos el componente de la lsita de resultado de enlaces por filtros
 const LinksListComponent = (props) => {
-    const { links } = props;
+    const { links, seeMore, onDelete, onUpdate } = props;
     return (
         <div className="results-list-container">
                 {links.map(link => (
             <div key={link._id} className="link-card">
                     <h3>{link.title}</h3>
-                    <p>{link.description}</p>
+                    <p>Descripcion: {link.description}</p>
+                    <button className="tag-button" onClick={() => seeMore(link._id)}>Ver mas</button>
+                    <button className="tag-button" onClick={() => onUpdate(link._id)}>Editar</button>
+                    <button onClick={() => onDelete(link._id)}>Eliminar</button>
             </div>
                 ))}
         </div>
