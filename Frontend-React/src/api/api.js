5bd3136b4538b02e81124179232988ebea6b9016
Victor Nunez <tu_correo@gmail.com>
2026-05-20 19:41:19 -0300

    feat: agregar funciones para obtener del backend los datos de un enlace, eliminar un enlace y actualizar un enlace de la base de datos

--- Frontend-React/src/api/api.js
+++ Frontend-React/src/api/api.js
@@ -8,7 +8,7 @@ const getTags = async () => {
     
     } catch (error) {
         console.error('Error al obtener etiquetas: ', error.message);
-        throw Error; // Relanzamos el error para que el componente que lo llame reciba el error tambien.
+        throw error; // Relanzamos el error para que el componente que lo llame reciba el error tambien.
     };
 }
 
@@ -29,7 +29,60 @@ const getLinks = async (id = 'Todos') => {
     
     } catch (error) {
         console.error('Error al obtener enlaces: ', error.message);
-        throw Error; // Relanzamos el error para que el componente que lo llame reciba el error tambien.
+        throw error; // Relanzamos el error para que el componente que lo llame reciba el error tambien.
     };
 }
-export { getTags, getLinks };
+
+const getLinksDetails = async (id) => {
+    try {
+        const response = await fetch(`http://localhost:3000/app/details/${id}`);
+        if(!response.ok) throw new Error('Hubo un error al obtener los detalles del enlace');
+        
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error('Error al obtener la info del enlace: ', error.message);
+        throw error;
+    }
+};
+
+const deleteLink = async (id) => {
+    try {
+        const response = await fetch(`http://localhost:3000/api/delete/${id}`, {
+            method: 'DELETE'
+        });
+
+        if(!response.ok) throw new Error('Hubo un error al intentar eliminar el enlace');
+
+        const data = await response.json();
+        return data;
+    
+    } catch(error) {
+        console.error('Hubo un error al eliminar el enlace: ', error.message);
+        throw error;
+    }
+};
+
+const updateLink = async (id, title, url, description) => {
+    try {
+        const response = await fetch(`http://localhost:3000/api/update/${id}`, {
+            method: 'PUT', 
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(title, url, description)
+            });
+
+        if(!response.ok) throw new Error('No se pudo actualizar los datos del enlace');
+
+        const data = await response.json();
+        return data;
+    
+    } catch (error) {
+        console.error('Hubo un error al actualizar los datos del enlace: ', error.message);
+        throw error;
+    }
+}
+
+export { getTags, getLinks, getLinksDetails, deleteLink, updateLink };
