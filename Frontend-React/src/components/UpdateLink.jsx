49f5af5e596b015c17baa83da5d865b4514ea191
Victor Nunez <tu_correo@gmail.com>
2026-05-20 19:36:10 -0300

    feat: crear componente de formulario de actualizacion de datos de un enlace (falta terminar)

---
+++ Frontend-React/src/components/UpdateLink.jsx
@@ -0,0 +1,22 @@
+/* import { useState, useEffect } from "react";
+import { getLinksDetails, updateLink } from "../api/api";
+
+/* const updateLinkComponent = (props) => {
+    const { linkId, onLinkUpdate } = props;
+
+    const [title, setTitle] = useState("");
+    const [url, setUrl] = useState("");
+    const [description, setDescription] = useState("")
+
+    useEffect(() => {
+        const loadLinkData = async () => {
+            try {
+                const data = await getLinksDetails(linkId);
+
+            }
+        }
+    })
+}  */ 
+
+
+    // FALTA TERMINAR EL COMPONENTE DE EDITAR UN ENLACE.
