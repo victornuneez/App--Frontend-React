0acf8d2082ee80255806bae0e08360b77df212a2
Victor Nunez <tu_correo@gmail.com>
2026-05-20 19:32:56 -0300

    feat: agregar funcionalidad al boton de filtro todos

--- Frontend-React/src/components/TagsFilter.jsx
+++ Frontend-React/src/components/TagsFilter.jsx
@@ -1,13 +1,13 @@
 
 // Creamos el componente de filtro por etiquetas
 const TagsFilterComponent = (props) => {
-    const { tags } = props;
-    const { filter } = props;
+    const { tags, filter } = props;
+
     {/*Regla de oro de listas en react, cada vez que utilizemos map para generar elementos repetidos, debe tener un key que lo identifique, asi react usa ese key internamente para saber exactamente que elemento cambio */}
     {/* Los parentesis dentro de onClick le dice a react que guarde la funcion filter y la ejecute cuando ocurra un click */}
     return (
         <div className="tags-filter-container">
-            <button key="Todos" className="tag-button">Todos</button> 
+            <button className="tag-button" onClick={() => filter("Todos")}>Todos</button> 
             {tags.map(tag => (
                 <button key={tag._id} className="tag-button" onClick={() => filter(tag._id)}>
                     {tag.name}
@@ -19,4 +19,3 @@ const TagsFilterComponent = (props) => {
 
 export default TagsFilterComponent;
 
-// NOTA: EL BOTON DE FILTRAR POR TODOS AUN NO FUNCIONA.
