a7d513681422c2394598a9da4ed1ee00c451540d
Victor Nunez <tu_correo@gmail.com>
2026-05-22 20:22:57 -0300

    feat: agrega condicional para mostrar un mensaje en pantalla si no hay resultados para el filtro marcado - anhade boton de crear enlace al final de la lista de resultados

--- Frontend-React/src/components/LinksList.jsx
+++ Frontend-React/src/components/LinksList.jsx
@@ -1,20 +1,31 @@
 
 // Creamos el componente de la lsita de resultado de enlaces por filtros
 const LinksListComponent = (props) => {
-    const { links, seeMore, onDelete, onUpdate } = props;
-    return (
-        <div className="results-list-container">
-                {links.map(link => (
-            <div key={link._id} className="link-card">
-                    <h3>{link.title}</h3>
-                    <p>Descripcion: {link.description}</p>
-                    <button className="tag-button" onClick={() => seeMore(link._id)}>Ver mas</button>
-                    <button className="tag-button" onClick={() => onUpdate(link._id)}>Editar</button>
-                    <button className="tag-button" onClick={() => onDelete(link._id)}>Eliminar</button>
+    const { links, seeMore, onCreate } = props;
+    
+    if(links.length === 0) {
+        return (
+            <p>No hay resultados</p>
+        )
+    
+    } else {
+        return (
+            <div className="results-list-container">
+                    {links.map(link => (
+                <div key={link._id} className="link-card">
+                        <h3>{link.title}</h3>
+                        <p>Descripcion: {link.description}</p>
+                        <button className="tag-button" onClick={() => seeMore(link._id)}>Ver mas</button>
+                </div>
+                    ))}
+                <br></br>
+                <div>
+                    <h2>Agrega un nuevo enlace</h2>
+                    <button onClick={onCreate}>Crear Enlace</button>
+                </div>
             </div>
-                ))}
-        </div>
-    );
+        );
+    }
 }
 
 export default LinksListComponent;
